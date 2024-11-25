71a29852e01f0b693874c71acf597b7dc2561ac5
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2024-11-25 18:41:27 -0500

    Line 28 Playercam script
    fixed rotational y bug

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -6,7 +6,6 @@ public class PlayerCam : MonoBehaviour
 {
     public float sensX;
     public float sensY;
-    //public GameObject gun;
     public Transform orientation;
 
     float xRotation;
@@ -23,12 +22,13 @@ public class PlayerCam : MonoBehaviour
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-        //gun.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         yRotation += mouseX;
-
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
+        //BUG FIX HERE 
+        Vector3 zScale = new Vector3(1,1,Mathf.Abs(xRotation/45)+1);
+        transform.localScale = zScale;
+        //
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
