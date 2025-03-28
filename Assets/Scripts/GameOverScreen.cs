44f8c63785ce18373a180d573c3bcc3280eeab93
AliC13 <alichy@umich.edu>
2025-03-28 19:34:19 -0400

    Levels Connected, Difficulty and Camera Locations Adjusted

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -84,6 +84,103 @@ public class GameOverScreen : MonoBehaviour
             case 4:
                 previousLevelName = "Level4Screen";
                 break;
+            case 5:
+                previousLevelName = "Level5Screen";
+                break;
+            case 6:
+                previousLevelName = "Level6Screen";
+                break;
+            case 7:
+                previousLevelName = "Level7Screen";
+                break;
+            case 8:
+                previousLevelName = "Level8Screen";
+                break;
+            case 9:
+                previousLevelName = "Level9Screen";
+                break;
+            case 10:
+                previousLevelName = "Level10Screen";
+                break;
+            case 11:
+                previousLevelName = "Level11Screen";
+                break;
+            case 12:
+                previousLevelName = "Level12Screen";
+                break;
+            case 13:
+                previousLevelName = "Level13Screen";
+                break;
+            case 14:
+                previousLevelName = "Level14Screen";
+                break;
+            case 15:
+                previousLevelName = "Level15Screen";
+                break;
+            case 16:
+                previousLevelName = "Level16Screen";
+                break;
+            case 17:
+                previousLevelName = "Level17Screen";
+                break;
+            case 18:
+                previousLevelName = "Level18Screen";
+                break;
+            case 19:
+                previousLevelName = "Level19Screen";
+                break;
+            case 20:
+                previousLevelName = "Level20Screen";
+                break;
+            case 21:
+                previousLevelName = "Level21Screen";
+                break;
+            case 22:
+                previousLevelName = "Level22Screen";
+                break;
+            case 23:
+                previousLevelName = "Level23Screen";
+                break;
+            case 24:
+                previousLevelName = "Level24Screen";
+                break;
+            case 25:
+                previousLevelName = "Level25Screen";
+                break;
+            case 26:
+                previousLevelName = "Level26Screen";
+                break;
+            case 27:
+                previousLevelName = "Level27Screen";
+                break;
+            case 28:
+                previousLevelName = "Level28Screen";
+                break;
+            case 29:
+                previousLevelName = "Level29Screen";
+                break;
+            case 30:
+                previousLevelName = "Level30Screen";
+                break;
+            case 31:
+                previousLevelName = "Level31Screen";
+                break;
+            case 32:
+                previousLevelName = "Level32Screen";
+                break;
+            case 33:
+                previousLevelName = "Level33Screen";
+                break;
+            case 34:
+                previousLevelName = "Level34Screen";
+                break;
+            case 35:
+                previousLevelName = "Level35Screen";
+                break;
+            case 36:
+                previousLevelName = "Level36Screen";
+                break;
+
             default:
                 previousLevelName = "Western_Env";
                 break;
@@ -107,6 +204,102 @@ public class GameOverScreen : MonoBehaviour
                 nextLevelName = "Level4Screen";
                 break;
             case 5:
+                nextLevelName = "Level5Screen";
+                break;
+            case 6:
+                nextLevelName = "Level6Screen";
+                break;
+            case 7:
+                nextLevelName = "Level7Screen";
+                break;
+            case 8:
+                nextLevelName = "Level8Screen";
+                break;
+            case 9:
+                nextLevelName = "Level9Screen";
+                break;
+            case 10:
+                nextLevelName = "Level10Screen";
+                break;
+            case 11:
+                nextLevelName = "Level11Screen";
+                break;
+            case 12:
+                nextLevelName = "Level12Screen";
+                break;
+            case 13:
+                nextLevelName = "Level13Screen";
+                break;
+            case 14:
+                nextLevelName = "Level14Screen";
+                break;
+            case 15:
+                nextLevelName = "Level15Screen";
+                break;
+            case 16:
+                nextLevelName = "Level16Screen";
+                break;
+            case 17:
+                nextLevelName = "Level17Screen";
+                break;
+            case 18:
+                nextLevelName = "Level18Screen";
+                break;
+            case 19:
+                nextLevelName = "Level19Screen";
+                break;
+            case 20:
+                nextLevelName = "Level20Screen";
+                break;
+            case 21:
+                nextLevelName = "Level21Screen";
+                break;
+            case 22:
+                nextLevelName = "Level22Screen";
+                break;
+            case 23:
+                nextLevelName = "Level23Screen";
+                break;
+            case 24:
+                nextLevelName = "Level24Screen";
+                break;
+            case 25:
+                nextLevelName = "Level25Screen";
+                break;
+            case 26:
+                nextLevelName = "Level26Screen";
+                break;
+            case 27:
+                nextLevelName = "Level27Screen";
+                break;
+            case 28:
+                nextLevelName = "Level28Screen";
+                break;
+            case 29:
+                nextLevelName = "Level29Screen";
+                break;
+            case 30:
+                nextLevelName = "Level30Screen";
+                break;
+            case 31:
+                nextLevelName = "Level31Screen";
+                break;
+            case 32:
+                nextLevelName = "Level32Screen";
+                break;
+            case 33:
+                nextLevelName = "Level33Screen";
+                break;
+            case 34:
+                nextLevelName = "Level34Screen";
+                break;
+            case 35:
+                nextLevelName = "Level35Screen";
+                break;
+            case 36:
+                nextLevelName = "Level36Screen";
+                break;
+            case 37:
                 nextLevelName = "TitleScreen";
                 break;
             default:
--- Assets/Scripts/GoToLevel.cs
+++ Assets/Scripts/GoToLevel.cs
@@ -48,6 +48,102 @@ public class GoToLevel : MonoBehaviour
             case "Level4Screen":
                 nextLevelName = "Level_4";
                 break;
+            case "Level5Screen":
+                nextLevelName = "Level_5";
+                break;
+            case "Level6Screen":
+                nextLevelName = "Level_6";
+                break;
+            case "Level7Screen":
+                nextLevelName = "Level_7";
+                break;
+            case "Level8Screen":
+                nextLevelName = "Level_8";
+                break;
+            case "Level9Screen":
+                nextLevelName = "Level_9";
+                break;
+            case "Level10Screen":
+                nextLevelName = "Level_10";
+                break;
+            case "Level11Screen":
+                nextLevelName = "Level_11";
+                break;
+            case "Level12Screen":
+                nextLevelName = "Level_12";
+                break;
+            case "Level13Screen":
+                nextLevelName = "Level_13";
+                break;
+            case "Level14Screen":
+                nextLevelName = "Level_14";
+                break;
+            case "Level15Screen":
+                nextLevelName = "Level_15";
+                break;
+            case "Level16Screen":
+                nextLevelName = "Level_16";
+                break;
+            case "Level17Screen":
+                nextLevelName = "Level_17";
+                break;
+            case "Level18Screen":
+                nextLevelName = "Level_18";
+                break;
+            case "Level19Screen":
+                nextLevelName = "Level_19";
+                break;
+            case "Level20Screen":
+                nextLevelName = "Level_20";
+                break;
+            case "Level21Screen":
+                nextLevelName = "Level_21";
+                break;
+            case "Level22Screen":
+                nextLevelName = "Level_22";
+                break;
+            case "Level23Screen":
+                nextLevelName = "Level_23";
+                break;
+            case "Level24Screen":
+                nextLevelName = "Level_24";
+                break;
+            case "Level25Screen":
+                nextLevelName = "Level_25";
+                break;
+            case "Level26Screen":
+                nextLevelName = "Level_26";
+                break;
+            case "Level27Screen":
+                nextLevelName = "Level_27";
+                break;
+            case "Level28Screen":
+                nextLevelName = "Level_28";
+                break;
+            case "Level29Screen":
+                nextLevelName = "Level_29";
+                break;
+            case "Level30Screen":
+                nextLevelName = "Level_30";
+                break;
+            case "Level31Screen":
+                nextLevelName = "Level_31";
+                break;
+            case "Level32Screen":
+                nextLevelName = "Level_32";
+                break;
+            case "Level33Screen":
+                nextLevelName = "Level_33";
+                break;
+            case "Level34Screen":
+                nextLevelName = "Level_34";
+                break;
+            case "Level35Screen":
+                nextLevelName = "Level_35";
+                break;
+            case "Level36Screen":
+                nextLevelName = "Level_36";
+                break;
             default:
                 nextLevelName = "Western_Env";
                 break;
